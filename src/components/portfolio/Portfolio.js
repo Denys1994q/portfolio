ee24b324ce94334e07c81322ae222d79d1f503c1
Denys1994q <drybachok@gmail.com>
2022-09-20 22:19:33 +0300

    correct links

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -13,8 +13,8 @@ const Portfolio = () => {
 
     const projects = [
         {name: 'Pets', description: '', img: pets, deploy: 'https://petsapi1994.herokuapp.com/', github: 'https://github.com/Denys1994q/test-task-CatsAPI', technologies: ['react', 'react-router-dom', 'node-sass', 'less', 'font-awesome', 'flex', 'grid', 'git', 'fetch', 'cat-api', 'heroku']},
-        {name: 'Fc Arsenal', description: '', img: arsenal, deploy: '', github: 'https://github.com/Denys1994q/Arsenal-React', technologies: ['react', 'react-router-dom', 'sass', 'flex', 'grid', 'git', 'fetch', 'omdb-api', 'weather-api', 'marvel-api', 'heroku', 'videoCINEMA', 'react-image-magnify']},
-        {name: 'Chat', description: '', img: chat, deploy: '', github: 'https://github.com/Denys1994q/chat_React', technologies: ['react', 'node-sass', 'sass', 'flex', 'git', 'fetch', 'chuck-norris-jokes-api', 'react-google-login', 'gapi-script', 'heroku']},
+        {name: 'Fc Arsenal', description: '', img: arsenal, deploy: '', github: 'https://github.com/Denys1994q/Arsenal-React', technologies: ['react', 'react-router-dom', 'sass', 'flex', 'grid', 'git', 'fetch', 'omdb-api', 'weather-api', 'marvel-api', 'heroku', 'react-image-magnify']},
+        {name: 'Chat', description: '', img: chat, deploy: 'https://chat199.herokuapp.com/', github: 'https://github.com/Denys1994q/chat_React', technologies: ['react', 'node-sass', 'sass', 'flex', 'git', 'fetch', 'chuck-norris-jokes-api', 'react-google-login', 'gapi-script', 'heroku']},
         {name: 'Marvel', description: '', img: marvel, deploy: 'https://marvel-comics19.herokuapp.com/', github: 'https://github.com/Denys1994q/Project_React_marvelComics', technologies: ['react', 'node-sass', 'less', 'react-transition-group', 'flex', 'git', 'fetch', 'helmet', 'marvel-api', 'heroku']},
         {name: 'Movies API', description: '', img: movies, deploy: '', github: 'https://github.com/Denys1994q/React-Movies-API', technologies: ['react', 'materialize-css', 'flex', 'git', 'fetch', 'omdb-api']},
         {name: 'currency converter', description: '', img: converter, deploy: 'https://currency-converter111.herokuapp.com/', github: 'https://github.com/Denys1994q/currency_converter_REACT', technologies: ['react', 'flex', 'git', 'fetch', 'currencies-api', 'heroku']},
@@ -34,9 +34,9 @@ const Portfolio = () => {
                     <div className='potrfolio-grid-item-img'>
                         <img className='potrfolio-grid-item-img-show' src={item.img} alt="" />
                         <div className='potrfolio-grid-item-img-title'>
-                            <a href={item.deploy} target='_blank'>
+                            {item.deploy !== '' ? <a href={item.deploy} target='_blank'>
                                 <span className='potrfolio-grid-item-img-title-span'>{item.name}</span>
-                            </a>
+                            </a> : <span className='potrfolio-grid-item-img-title-span'>{item.name}</span> }
                         </div>
                     </div>
                     <ul className='potrfolio-grid-item-tecnhs'>
